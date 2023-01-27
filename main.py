156ed2b0a23a121ef963569d1d427f5b7cce1ffa
atrox3d <atrox3d@gmail.com>
2023-01-27 08:01:51 +0100

    improve comments

--- main.py
+++ main.py
@@ -11,60 +11,77 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
+# push command line argument for testing in pycharm
 sys.argv.append('d:/downloads')
+
+# get path from command line argument or work from cwd
 try:
     root = sys.argv[1]
 except IndexError:
     root = '.'
-root = Path(root).absolute()
 
+# set root path
+root = Path(root).absolute()
 log.info(f'{root=}')
 
+# get list of all .srt files
 srts = list(root.glob('**/*.srt'))
 
+# group srt files by path:
 # create dictionary path -> list[ dict(filename, filesize) ]
 groups = {}
 for srt in srts:
+    # select parent (movies), or parent of parent (tvshows), of srt file
     if srt.parent.name.lower() == 'subs' or srt.parent.parent.name.lower() == 'subs':
+        # store srt filename and size in temp dict
         filedict = dict(name=srt.name, size=srt.stat().st_size)
+        #  create or update list of srt files
         if groups.get(srt.parent, None):
             groups[srt.parent].append(filedict)
         else:
             groups[srt.parent] = [filedict]
 
-# flatten list to the bigger file
+# flatten list of srt files to the bigger file
 for path, files in groups.items():
     bigger = None
     max_size = 0
     for file in files:
         if file['size'] > max_size:
             bigger = file['name']
-
     groups[path] = bigger
 
+# loop through flattened groups of srt files
 for path, file in groups.items():
+    # create source path for copy
     src = path / file
 
     if path.name.lower() == 'subs':
+        # rename srt files based on movie name
         # if .../moviename/subs/file.srt
 
-        # .../moviename/
+        # set destination path to movie folder
         dest_path = path.parent
-        # moviename
+
+        # set destination filename to movie folder name + .srt
         dest_name = path.parent.parts[-1] + '.srt'
-        # .../moviename/moviename.srt
+
+        # create destination srt path
         dst = dest_path / dest_name
     elif path.parent.name.lower() == 'subs':
+        # rename srt files based on tvshow episode name
         # if .../seasonname/subs/episodename/file.srt
 
-        # .../seasonname/
+        # set destination path to tvshow folder
         dest_path = path.parent.parent
-        # episodename
+
+        # set destination filename to subtitle folder name + .srt
         dest_name = path.parts[-1] + '.srt'
-        # .../season/episodename.srt
+
+        # create destination srt path
         dst = dest_path / dest_name
     else:
-        raise ValueError(f'unknown path structure: {path / file}')
+        # implement new structure if exception is arisen
+        raise ValueError(f'NOT IMPLEMENTED | unknown path structure: {path / file}')
 
     log.info(f'COPY | SRC | {src}')
     log.info(f'COPY | DST | {dst}')
