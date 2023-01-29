40805253168c8f000f871a85c5c887cd95a47667
atrox3d <atrox3d@gmail.com>
2023-01-29 08:02:23 +0100

    refactor logger initialization

--- modules/logger.py
+++ modules/logger.py
@@ -8,10 +8,21 @@ def get_logger(name):
     :param name:
     :return:
     """
+    funcname_width = 12
+    levelname_width = str(len("CRITICAL"))
+
+    line_format = f"%(asctime)s | " \
+                  f"%(module)s.py | " \
+                  f"%(funcName){funcname_width}s() | " \
+                  f"%(levelname)-{levelname_width}s | " \
+                  f"%(message)s"
+
+    date_format = '%Y/%m/%d %H:%M:%S'
+
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s | %(module)s | %(levelname)-" + str(len("CRITICAL")) + "s | %(message)s",
-        datefmt='%Y/%m/%d %H:%M:%S',
+        format=line_format,
+        datefmt=date_format,
         stream=sys.stdout
     )
     logger = logging.getLogger(name)
